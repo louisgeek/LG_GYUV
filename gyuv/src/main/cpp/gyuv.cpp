6e96c1e439d973ff33617fca3fcc343bc3307854
louisgeek <louisgeek@qq.com>
2022-04-09 00:08:18 +0800

    update

--- gyuv/src/main/cpp/gyuv.cpp
+++ gyuv/src/main/cpp/gyuv.cpp
@@ -1,8 +1,14 @@
+//
+// Created by louisgeek on 2022/4/7.
+//
 #include <jni.h>
 //
-#include "libyuv.h"
-
-
+#include "gyuvc.h"
+//
+//#include <android/log.h>
+//
+//#define TAG    "gyuvc"
+//#define LOGD(...)  __android_log_print(ANDROID_LOG_DEBUG,TAG,__VA_ARGS__)
 
 
 
@@ -10,19 +16,23 @@ extern "C"
 JNIEXPORT void JNICALL
 Java_com_louisgeek_gyuv_GYUV_NV21ToI420(JNIEnv *env, jobject thiz, jbyteArray nv21_src,
                                         jint width_src, jint height_src, jbyteArray i420_dst) {
-    //libyuv.h
-    libyuv::NV21ToI420(src, dst, width_src, height_src);
+    jbyte *src = env->GetByteArrayElements(nv21_src, NULL);
+    jbyte *dst = env->GetByteArrayElements(i420_dst, NULL);
+    //
+    NV21ToI420(src, width_src, height_src, dst);
+    //释放
+    env->ReleaseByteArrayElements(nv21_src, src, 0);
+    env->ReleaseByteArrayElements(i420_dst, dst, 0);
 }
 
 extern "C"
 JNIEXPORT void JNICALL
 Java_com_louisgeek_gyuv_GYUV_I420ToNV21(JNIEnv *env, jobject thiz, jbyteArray i420_src,
                                         jint width_src, jint height_src, jbyteArray nv21_dst) {
-
     jbyte *src = env->GetByteArrayElements(i420_src, NULL);
     jbyte *dst = env->GetByteArrayElements(nv21_dst, NULL);
-    //libyuv.h
-    libyuv::I420ToNV21(src, dst, width, height);
+    //
+    I420ToNV21(src, width_src, height_src, dst);
     //释放
     env->ReleaseByteArrayElements(i420_src, src, 0);
     env->ReleaseByteArrayElements(nv21_dst, dst, 0);
@@ -34,19 +44,23 @@ extern "C"
 JNIEXPORT void JNICALL
 Java_com_louisgeek_gyuv_GYUV_NV12ToI420(JNIEnv *env, jobject thiz, jbyteArray nv12_src,
                                         jint width_src, jint height_src, jbyteArray i420_dst) {
-    //libyuv.h
-    libyuv::NV12ToI420(src, dst, width_src, height_src);
+    jbyte *src = env->GetByteArrayElements(nv12_src, NULL);
+    jbyte *dst = env->GetByteArrayElements(i420_dst, NULL);
+    //
+    NV12ToI420(src, width_src, height_src, dst);
+    //释放
+    env->ReleaseByteArrayElements(nv12_src, src, 0);
+    env->ReleaseByteArrayElements(i420_dst, dst, 0);
 }
 
 extern "C"
 JNIEXPORT void JNICALL
 Java_com_louisgeek_gyuv_GYUV_I420ToNV12(JNIEnv *env, jobject thiz, jbyteArray i420_src,
                                         jint width_src, jint height_src, jbyteArray nv12_dst) {
-
     jbyte *src = env->GetByteArrayElements(i420_src, NULL);
     jbyte *dst = env->GetByteArrayElements(nv12_dst, NULL);
-    //libyuv.h
-    libyuv::I420ToNV12(src, dst, width_src, height_src);
+    //
+    I420ToNV12(src, width_src, height_src, dst);
     //释放
     env->ReleaseByteArrayElements(i420_src, src, 0);
     env->ReleaseByteArrayElements(nv12_dst, dst, 0);
---
+++ gyuv/src/main/cpp/gyuvc.h
@@ -0,0 +1,16 @@
+//
+// Created by louisgeek on 2022/4/9.
+//
+
+#ifndef LG_GYUV_GYUVC_H
+#define LG_GYUV_GYUVC_H
+
+#endif //LG_GYUV_GYUVC_H
+
+void I420ToNV12(jbyte *src, jint width, jint height, jbyte *dst)
+
+void NV12ToI420(jbyte *src, jint width, jint height, jbyte *dst)
+
+void I420ToNV21(jbyte *src, jint width, jint height, jbyte *dst)
+
+void NV21ToI420(jbyte *src, jint width, jint height, jbyte *dst)
